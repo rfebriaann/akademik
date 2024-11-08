b3e2517d0982f8cc61468301ba5f6e3ce1831598
Rafli Febrian <raflyfebriann@gmail.com>
2024-11-08 22:16:57 +0700

    update layout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -14,6 +14,7 @@
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="/build/assets/app-Cf0ZaRbz.css">
         @vite('resources/css/app.css')
         @stack('styles')
     </head>
